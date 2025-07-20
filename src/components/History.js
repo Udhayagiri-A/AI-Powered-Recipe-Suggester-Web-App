b5c5314c2c7ab4ee6a02ccfd6af3cd1d109872cf
Udhayagiri-A <udhayagiriayyavu@gmail.com>
2025-07-20 19:41:35 +0530

    Deleted all.

--- src/components/History.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function History() {
-  return (
-    <div>
-      Welcome to the History Page
-    </div>
-  )
-}
--- src/components/ViewDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ViewDetails() {
-  return (
-    <div>
-      Welcome to the ViewDetails Page
-    </div>
-  )
-}
